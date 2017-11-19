debae8144b431fa28ef02bbd9999a5ec0c8ae3fc
Judith <jirg@yopmail.com>
2017-11-19 15:04:56 +0800

    connect

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php 
+	//<!------------------- Judith 2017/11/19 修改 -------!> ///
+	header('Content-Type: text/html; charset=utf-8');
+	mysql_query("SET NAMES 'UTF8'"); //設定語系
+	$sql=mysql_connect("localhost", "root", "12345")or die("cannot connect");//連接資料庫
+	$db=mysql_select_db("limesurvey")or die("cannot connect DB");
+
+	if($sql){
+		echo '連接成功'.'<br>';
+	}else{
+		echo '連接失敗'.'<br>';
+		exit;
+	}
+
+	if($db){
+		echo '連接成功'.'<br>';
+	}else{
+		echo '連接失敗'.'<br>';
+		exit;
+	}
+?>
