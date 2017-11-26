5ed2dfbbc7fa86b482b35acb529ad0c465995f6c
asd171119 <dollable0522@gmail.com>
2017-11-26 15:04:28 +0800

    2017/11/26

--- result.php
+++ result.php
@@ -1,16 +1,14 @@
-<!----Rhoda 2017/11/25 修改---->
+<!----Rhoda 2017/11/26 修改---->
+<?php
+	include('connect.php');
+?>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>無標題文件</title>
-<?php
-	include('connect.php');
-?>
+<title>問卷統計表</title>
 </head>
-
 <body>
-
 	<table width="300" border="0" style="border:1px #000000 solid;text-align: center;" RULES=ALL>
   <tbody>
     <tr>
@@ -19,21 +17,17 @@
     </tr>
     <?php 
 	$date="SELECT * FROM `lime_survey_12` WHERE `submitdate` is not null";
-    $result = mysql_query($date) or die('query error0');
-	$count_date=mysql_num_rows($result); 
+      $result = mysql_query($date);
+      $row= mysqli_num_rows($result);
+      //if(mysqli_num_rows($result) ==0)   exit;
+      while($r = mysql_fetch_assoc($result)){
 	  
-    for($i=0;$i<$count_date;$i++){ 
-		$array[$i]=mysql_fetch_array($result);
-		//echo $array[$i]['id'];
-	 };?>
-
-	  
-	  <?php for($i=0;$i<$count_date;$i++){ ?>
-    <tr>
-      <td><?php echo $array[$i]['id'];?></td>
-      <td><?php echo $array[$i]['submitdate'];?></td>
-    </tr>
-    <?php } ?>
+      ?>
+      <tr>
+        <td><?php echo $r['id']; ?></td>
+        <td><?php echo $r['submitdate']?></td>
+      </tr>
+      <?php }?>
   </tbody>
 </table>
 
