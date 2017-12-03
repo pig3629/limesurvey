b36414acf78c66e9b252a53b7794730dc6bb9b1f
asd171119 <33798020+asd171119@users.noreply.github.com>
2017-12-03 22:17:41 +0800

    2017/12/03

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
-<!----Rhoda 2017/12/01 修改---->
+<!----Rhoda 2017/12/03 修改---->
 <?php
 include( 'connect.php' );
 ?>
@@ -19,7 +19,7 @@ include( 'connect.php' );
 		$sql="SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS Where Table_Name ='lime_survey_12' and DATA_TYPE not like 'datetime'";
 		$result = mysql_query($sql);
 		$count_bar=mysql_num_rows($result); 
-		
+		echo $count_bar;
     	for($i=0;$i<$count_bar;$i++){ 
 			$array_bar[$i]=mysql_fetch_array($result);
 	 	};
@@ -46,13 +46,13 @@ include( 'connect.php' );
 				$Arr1=str_split($array_bar[$i]['COLUMN_NAME']);
 				$Arr2=str_split($array_bar[$j]['COLUMN_NAME']); 
 				$aa=$Arr1[5].$Arr1[6].$Arr1[7];
-				$bb=$Arr2[5].$Arr2[6].$Arr2[7];;
+				$bb=$Arr2[5].$Arr2[6].$Arr2[7];
 			}else {
 				$j=$i+1;
 				$Arr1=str_split($array_bar[$i]['COLUMN_NAME']);
 				$Arr2=str_split($array_bar[$j]['COLUMN_NAME']); 
 				$aa=$Arr1[6].$Arr1[7].$Arr1[8];
-				$bb=$Arr2[6].$Arr2[7].$Arr2[8];;
+				$bb=$Arr2[6].$Arr2[7].$Arr2[8];
 			 }
 			if($aa==$bb){
 				$i+1;
@@ -60,31 +60,36 @@ include( 'connect.php' );
 				$sql_x="SELECT * FROM `lime_questions` WHERE `qid`='$aa' AND `language`='zh-Hant-TW'";
 				$result_x = mysql_query($sql_x);
 				while($w_x = mysql_fetch_assoc($result_x)){	
-				if($w_x['type']=='M'){
-					$sql_title="SELECT * FROM `lime_questions` WHERE `parent_qid`='$aa' AND `language`='zh-Hant-TW'";
-					$result_title = mysql_query($sql_title);
-					while($w_title = mysql_fetch_assoc($result_title)){
+					if($w_x['type']=='M'||$w_x['type']=='F'){
+						$sql_title="SELECT * FROM `lime_questions` WHERE `parent_qid`='$aa' AND `language`='zh-Hant-TW'";
+						$result_title = mysql_query($sql_title);
+						while($w_title = mysql_fetch_assoc($result_title)){
+							echo "<td>";
+							echo $w_x['question'].$w_title['question'];
+							echo "</td>";	
+						}
+					}else{
+							echo "<td>";
+							echo $w_x['question'];
+							echo "</td>";
+						}
+					
+					if($w_x['other']=='Y'){
 						echo "<td>";
-						echo $w_x['question'].$w_title['question'];
+						echo $w_x['question']."其他";
 						echo "</td>";
-						
 					}
-					echo "<td>";
-					echo $w_x['question']."其他";
-					echo "</td>";
-				}else{
-					echo "<td>";
-					echo $w_x['question'];
-					echo "</td>";
-				 }
+				 	}
+				
 			}	
 	 	}
-		}
+		
 		
 		
 		//表格內容
 		$sql="SELECT * FROM `lime_survey_12` WHERE `submitdate` is not null ";
 		$result = mysql_query($sql);
+		
 		while($rs = mysql_fetch_assoc($result)){
 			echo '<tr>';
 			foreach ($rs as $key=>$value){
